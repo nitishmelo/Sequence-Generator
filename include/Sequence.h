184dee2d36328662d51ccea605dfb6014feb84ae
nitishmelo <106487576+nitishmelo@users.noreply.github.com>
2022-05-29 14:06:09 -0400

    Added Header Files

---
+++ include/Sequence.h
@@ -0,0 +1,22 @@
+#include <vector>
+
+#define TERMS_RANGE 4
+#define FIRST_TERM_RANGE 99
+#define ADD_RANGE 10
+#define MUL_RANGE 9
+#define NUMTERMSDIV 2
+#define NUMMISSINGSUB 2
+
+struct Sequence
+{
+	std::vector<int> terms;
+	std::vector<int> solutions;
+	std::vector<int> missingindices;
+};
+
+struct Info
+{
+	int numofTerms;
+	int firstterm;
+	int constant;
+};
