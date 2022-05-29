69b28a47ad9247ff57e8075c1e40283812dad277
nitishmelo <106487576+nitishmelo@users.noreply.github.com>
2022-05-29 14:07:46 -0400

    Delete SeqFunct.h

--- SeqFunct.h
+++
@@ -1,9 +0,0 @@
-#include "Sequence.h"
-void Randominit(int info[2]);
-int RandomAddConstant();
-int RandomMultiplyConstant();
-void Randomindices(int numofTerms, std::vector<int> &missingindices);
-void Insert(Sequence &seq, int val, bool isMissing);
-void InsertTerms(Sequence& seq, std::vector<int> terms);
-void BasicSeqGen(Sequence& seq);
-void DigitGrouping(Sequence &seq);
