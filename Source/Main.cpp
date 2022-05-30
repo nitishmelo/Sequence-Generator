58271fa5b5a27a4e5a0a3a2300c15c0afcfcf232
nitishmelo <106487576+nitishmelo@users.noreply.github.com>
2022-05-30 16:38:30 -0400

    Update Main.cpp

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,22 +1,32 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "SeqFunct.h"
 #include "Printer.h"
+
+#define SEQUENCE_LIMIT 1000000
 
 int main()
 {
 	int seqs;
-	while (std::cout << "Enter the number of sequences to generate: " && !(std::cin >> seqs))
+	while (std::cout << "Enter the number of sequences to generate: (LIMIT: 1000000) " && (!(std::cin >> seqs) || (seqs > SEQUENCE_LIMIT || seqs < 0)))
 	{
 		std::cin.clear();
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		std::cout << std::endl << "Invalid input; try again: " << std::endl;
+		if (seqs > SEQUENCE_LIMIT)
+		{
+			std::cout << std::endl << "Exceeded sequence limit; try again: " << std::endl;
+		}
+		else
+		{
+			std::cout << std::endl << "Invalid input; try again: " << std::endl;
+		}
 		std::cout << std::endl;
 	}
 	std::cout << std::endl;
 	if (seqs == 0)
 	{
-		std::cout << "No sequences generated; any existing files with the same filenames will remain unaffected." << std::endl;
+		std::cout << "No sequences generated." << std::endl;
 		std::getchar();
 		std::cout << std::endl;
 		std::cout << "Press enter to end the program." << std::endl;
@@ -26,13 +36,23 @@ int main()
 	srand(time(NULL));
 	std::ofstream term;
 	std::ofstream sol;
-	term.open("seq.txt");
-	sol.open("solutions.txt");
+	std::string seqfile;
+	std::string solfile;
+	std::cout << "Enter a filename to write the sequences to: ";
+	std::cin >> seqfile;
+	std::cout << std::endl;
+	std::cout << "Enter a filename to write solutions to: ";
+	std::cin >> solfile;
+	std::cout << std::endl;
+	term.open(seqfile);
+	sol.open(solfile);
 	std::cout << "Generating & writing sequences and solutions..." << std::endl;
 	std::cout << std::endl;
 	PrintToFile(seqs, term, sol);
 	std::cout << "Sequences & solutions generated and written successfully." << std::endl;
 	std::getchar();
+	term.close();
+	sol.close();
 	std::cout << std::endl;
 	std::cout << "Press enter to end the program." << std::endl;
 	std::getchar();
