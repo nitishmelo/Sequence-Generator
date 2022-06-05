f712e3cc6d99e6f4cf4fcc01b826f0b133f6a77e
nitishmelo <106487576+nitishmelo@users.noreply.github.com>
2022-06-05 03:35:03 -0400

    Update SeqFunct.h

--- include/SeqFunct.h
+++ include/SeqFunct.h
@@ -3,11 +3,11 @@ void Randominit(int info[2]);
 int RandomAddConstant();
 int RandomMultiplyConstant();
 void Randomindices(int numofTerms, std::vector<int> &missingindices, bool isgeo);
-void RandomIndiceGen(std::vector<int>& missingindices, int numofTerms, int numofMissing);
+void RandomIndiceGen(std::vector<int> &missingindices, int numofTerms, int numofMissing);
 void Insert(Sequence &seq, int val, bool isMissing);
 void InsertTerms(Sequence &seq, std::vector<int> &terms);
-void GenerateTerms(std::vector<int> &terms, bool isgeo, int constant, int curr);
+void GenerateTerms(std::vector<int> &terms, bool isgeo, int constant, int curr, int extra);
 void BasicSeqGen(Sequence &seq);
 
-void DigitGrouping(Sequence &seq);
-int intlen(int num);
+int DigitGrouping(Sequence &seq);
+int CombineDigits(std::vector<int> &terms, std::vector<int> &combineddigits, int numofTerms, int numofDigits);
