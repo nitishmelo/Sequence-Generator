05afc30b8a499ab16863e273614247cfcc541c61
nitishmelo <106487576+nitishmelo@users.noreply.github.com>
2022-06-18 04:27:39 -0400

    Update SeqFunct.h

--- include/SeqFunct.h
+++ include/SeqFunct.h
@@ -1,18 +1,23 @@
 #include "Sequence.h"
+
 void Randominit(int info[2]);
 int RandomAddConstant();
 int RandomMultiplyConstant();
 void Randomindices(int numofTerms, std::vector<int> &missingindices, bool isgeo);
 void RandomIndiceGen(std::vector<int> &missingindices, int numofTerms, int numofMissing, int lowerrange);
-void Insert(Sequence &seq, int val, bool isMissing);
-void InsertTerms(Sequence &seq, std::vector<int> &terms);
+
+void Insert(Sequence &seq, std::string val, bool isMissing);
+void InsertTerms(Sequence &seq, std::vector<std::string> &terms);
+
 void GenerateTerms(std::vector<int> &terms, bool isgeo, int constant, int curr, int extra);
 void BasicSeqGen(Sequence &seq);
 
-int DigitGrouping(Sequence &seq);
+void DigitGrouping(Sequence &seq);
 void OneDigitList(std::vector<int> &terms, std::vector<int> &newlist);
 void CombineDigits(std::vector<int> &terms, std::vector<int> &combineddigits, int numofTerms, int numofDigits);
-int DetermineNumofDigits(std::vector<int> &newlist, Sequence &seq);
+void DetermineNumofDigits(std::vector<int> &newlist, Sequence &seq);
 
 void FibonacciHelper(std::vector<int> &terms);
-int Fibonacci(Sequence &seq);
+void Fibonacci(Sequence &seq);
+
+void DigitOperations(Sequence &seq);
