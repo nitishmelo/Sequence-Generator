b39cb24e5df5580ba203c5ed34fcf08d2739889d
nitishmelo <106487576+nitishmelo@users.noreply.github.com>
2022-06-18 04:26:44 -0400

    Update Printer.h

--- include/Printer.h
+++ include/Printer.h
@@ -1,3 +1,3 @@
 #include <fstream>
 void PrintToFile(int seqs, std::ostream &terms, std::ostream &sols);
-void PrintVal(int val, int len, bool isGrouping, std::ostream &stream);
+void PrintVal(int val, std::ostream &stream);
