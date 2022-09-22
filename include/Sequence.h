b7167f86da1395d71c7e6874569d1481db5ca9cd
nitishmelo <106487576+nitishmelo@users.noreply.github.com>
2022-09-22 01:37:26 -0400

    Update Sequence.h

--- include/Sequence.h
+++ include/Sequence.h
@@ -1,11 +1,11 @@
 #include <vector>
 #include <string>
 
-#define TERMS_RANGE 4
-#define FIRST_TERM_RANGE 100
-#define ADD_RANGE 50
+#define TERMS_RANGE 3
+#define FIRST_TERM_RANGE 50
+#define ADD_RANGE 20
 #define MUL_RANGE 9
-#define NUMTERMSDIV 2
+#define NUMTERMSDIV 3
 #define NUMMISSINGSUB 2
 
 struct Sequence
@@ -14,3 +14,4 @@ struct Sequence
 	std::vector<std::string> solutions;
 	std::vector<int> missingindices;
 };
+
